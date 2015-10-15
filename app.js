e2304c0e92e91c5c16939959260da47b92954ca7
chenwenxiao <chenwenxiao@live.cn>
2015-10-15 11:04:25 +0800

    install ace editor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// test b
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -14,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
---
+++ public/javascripts/editor.js
@@ -0,0 +1,6 @@
+define(['ace'], function() {
+  var editor = ace.edit("editor");
+      editor.setTheme("ace/theme/twilight");
+      editor.session.setMode("ace/mode/javascript");
+  return editor;
+});
