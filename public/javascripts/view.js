6154c4500eb83055370ba0267351dd0918acaa30
iamlockelightning <iamlockelightning@gmail.com>
2015-10-15 16:55:13 +0800

    main window

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,3 +1,29 @@
-define([], function() {
+define(['map', 'jquery', 'snapsvg'], function(map, $, snapsvg) {
+  function init(map) {
+    var svg = Snap("#svg");
+    var ground = svg.paper.rect(0, 0, 600, 600);
+    ground.attr({
+      fill: "#77FF00"
+    });
 
+    // for (var nitem in map.items) {
+    //   if (strife(nitem, item))
+    //     array.push(nitem);
+    // }
+    // return array;
+
+
+  };
+  function move(item, dx, dy) {
+
+  };
+  function use(item) {
+
+
+  };
+  return {
+    init : init,
+    move : move,
+    use : use
+  };
 });
