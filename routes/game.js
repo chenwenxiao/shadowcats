362b85ca1c33f0c688b946423750f596d423e02e
mfhraven <mfhraven@126.com>
2015-10-19 20:51:30 +0800

    v0.2.0

---
+++ routes/game.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+	res.render('index', { title: 'Express' });
+	// get Big Map;
+	//req.redirect('/');
+})
+
+//var map = require('../mapInfo/1-1.js');
+//map = {titile : '1-1', map};
+//map =JSON.stringify(map);
+//console.log(map);
+//map = JSON.parse(map);
+//console.log(JSON.stringify(map));
+
+/* 获取地图信息 */
+router.get('/1-1', function(req, res, next){
+	var map = require('../mapInfo/1-1');
+	res.render('index', { title: '1-1', map });	
+})
+
+router.get('/1-1/image/:id', function(req, res, next){
+	console.log(req.params);
+	
+});
+
+module.exports = router;
