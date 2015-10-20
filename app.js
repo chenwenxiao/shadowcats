c8f076ada56958fd3e8d8f43db9d91cc77307cec
chenwenxiao <chenwenxiao@live.cn>
2015-10-21 00:51:30 +0800

    use the animated-game as bigmap

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var editor = require('./routes/editor');
-var collie = require('./routes/collie');
+var bigmap = require('./routes/bigmap');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/editor', editor);
 app.use('/users', users);
-app.use('/collie', collie);
+app.use('/bigmap', bigmap);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
