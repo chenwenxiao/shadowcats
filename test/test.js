c257769916e20572862147485a66ba402974e76d
chenwenxiao <chenwenxiao@live.cn>
2015-10-22 14:20:14 +0800

    change for the website

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var add    = require('../public/javascripts/add.js');
-
-describe('add()', function() {
-  it('should return 2 when you pass it 1, 1', function() {
-    assert.equal(add(1, 1), 2);
-  });
-});
