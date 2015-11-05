8e4874abcf91cba1900acc367a6bfe0431e2a1d4
iamlockelightning <iamlockelightning@gmail.com>
2015-11-05 14:48:56 +0800

    fix player img in bigmap.

--- public/javascripts/mapInfo/1-3.js
+++ public/javascripts/mapInfo/1-3.js
@@ -7,7 +7,7 @@
       content : '向左走拨动开关开门：\n\
       solve.left 35\n\
       solve.use solve.map.items[3]\n\
-      items[3] is a ladder\n\'
+      items[3] is a ladder\n'
     },
     background : {
       src : "/images/1-3.png"
@@ -80,7 +80,7 @@
         type : 'fish',
         id : 4,
         x : 520,
-        y : 460,
+        y : 360,
         width : 80,
         height : 40,
         stable : false,
