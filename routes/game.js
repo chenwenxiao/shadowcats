997bc7c67c87ddd742bd99a84bf40be71c454b07
mfhraven <mfhraven@126.com>
2015-11-05 15:40:25 +0800

    fix the route bug

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* 获取地图信息 */
 router.get('/', function(req, res, next){
-	if (req.session.email){
+	if (req.session.email == null){
 		res.redirect('/login');
 	}else{
 		var map = require('../public/javascripts/mapInfo/' + req.query['stage']);
