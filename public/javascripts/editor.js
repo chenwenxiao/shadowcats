687b31807b471244a949ddd4ee342c9cbe17ed77
iamlockelightning <iamlockelightning@gmail.com>
2015-11-18 23:54:19 +0800

    Added comments on view.js, solve.js etc

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,4 +1,5 @@
 define(['ace/ace', 'jquery'], function(ace, $) {
+  // 代码编辑器初始化（属性设定）
   var editor = ace.edit("editor");
       editor.setTheme("ace/theme/twilight");
       editor.session.setMode("ace/mode/coffee");
