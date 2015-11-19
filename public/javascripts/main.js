eae7f647a90179ff4609f1087d49563b4fa2c1ce
chenwenxiao <chenwenxiao@live.cn>
2015-11-19 14:50:54 +0800

    Change the version of jquery

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@ require.config({
   paths: {
     ace: '../libs/ace-build/src',
 		bootstrap : '../libs/bootstrap/dist/js/bootstrap',
-		jquery : '../libs/jquery/dist/jquery',
+		jquery : '../libs/jquery/dist/jquery.min',
 		snapsvg : '../libs/Snap.svg/dist/snap.svg',
 		coffeescript : '../libs/coffee-script/extras/coffee-script',
     confirm: '../libs/jquery-confirm-action/jquery.confirm-action'
