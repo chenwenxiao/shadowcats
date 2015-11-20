b049752c6c1b2e058ae0236f4bf77eee136fb417
iamlockelightning <iamlockelightning@gmail.com>
2015-11-20 22:44:23 +0800

    Remove useless files.

--- public/images/game_files/editor.js
+++
@@ -1,7 +0,0 @@
-define(['ace/ace', 'jquery'], function(ace, $) {
-  // 代码编辑器初始化（属性设定）
-  var editor = ace.edit("editor");
-      editor.setTheme("ace/theme/twilight");
-      editor.session.setMode("ace/mode/coffee");
-  return editor;
-});
